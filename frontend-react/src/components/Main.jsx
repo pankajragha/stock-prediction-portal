238853783102a052d625d3775da370f8166a4a4e
Saurabh Tomar <pankajraghav9761@gmail.com>
2026-02-04 22:24:58 +0530

    homepage react

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+
+const Main = ()=>{
+    return(
+        <>
+        <div className="container">
+            <div className="p-5 text-center bg-light-dark rounded">
+                <h1 className="text-light">Stock Prediction Portal</h1>
+                <p className="text-light lead">The stock prediction portal util-izes machine learning techniques, specifically employing Keras, and LSTM model, integrated within the Django framework.It forecast future stock prices by analyzing 100-day and 200-day moving averages, essential indicators widely used by stock analysis to inform trading and investment decisions.</p>
+                <Button text="Login" class="btn-outline-info"/>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Main
