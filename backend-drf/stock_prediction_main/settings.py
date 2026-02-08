7cbe8b10603fd69edade633001df14a60b0da914
Saurabh Tomar <pankajraghav9761@gmail.com>
2026-02-08 10:37:18 +0530

    registration page

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'api',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS=[
+    'http://localhost:5173',
+]
+print("cors setting loaded")
---
+++ frontend-react/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Login = ()=>{
+    return(
+        <>
+        <h1 className="text-light container">Hii, PLease Login</h1>
+        </>
+    )
+}
+export default Login
